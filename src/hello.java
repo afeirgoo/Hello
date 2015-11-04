862959775357e98fd00b36293d5d37c174baee87
Guoyufei <g00133600@CHINA.HUAWEI.COM>
2015-11-04 09:49:23 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by g00133600 on 2015/11/3.
+ */
+public class hello {
+    /**
+     * 输出一行字符串“Hello World!”
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
